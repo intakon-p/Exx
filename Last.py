f9b21ff982a13721984a379f5d7401a408409f47
intakon-p <intakon.p@kkumail.com>
2024-04-11 00:05:16 +0700

    Update Last.py

--- Last.py
+++ Last.py
@@ -1,3 +1,26 @@
 from LastFuncCalMain import *
+import mimetypes
+from tkinter import *  
+import customtkinter
 
-video_pose_estimation2(0)
+root = customtkinter.CTk()
+
+root.geometry ("300x400")
+
+def webcam():
+    video_pose_estimation2(0)
+
+b1 = customtkinter.CTkButton(master = root, text = "Browse", command = webcam())
+
+b1.place(relx=0.5, rely=.5, anchor = CENTER)
+
+root. mainloop()
+
+
+
+
+
+
+mimetypes.init()
+
+# video_pose_estimation2(0)
