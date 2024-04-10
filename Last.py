f4bbd3c4ea12aef50d1f6553f8369d1fafac3fe5
D123568749 <87933093+D123568749@users.noreply.github.com>
2024-04-10 23:08:15 +0700

    aaa
    aaaaasssss

--- Last.py
+++ Last.py
@@ -1,3 +1,21 @@
 from LastFuncCalMain import *
+import mimetypes
+from tkinter import *  
+from tkinter import messagebox  
+from tkinter import filedialog
 
-video_pose_estimation2(0)
+mimetypes.init()
+root=Tk()
+variable1=StringVar()    
+variable2=StringVar()    
+
+root.geometry("800x800")
+
+l1 =Label(root, text = "Biomechanical Posture", font= ('Helvetica 25 bold')).place(relx=.5, rely=0,anchor= N)
+l2 =Label(root, textvariable = variable1, font= ('Helvetica 10 bold')).place(relx=.5, rely=.6,anchor= N)
+l3 =Label(root, textvariable = variable2, font= ('Helvetica 10 bold')).place(relx=.5, rely=.7,anchor= N)
+b1=Button(root,text="Browse for a video or an audio",font=40,command=video_pose_estimation2(0)
+).place(relx=.5, rely=.2,anchor= N)
+#b1=Button(root,text="Choose Live Posture Analysis using webcam",font=40,command=webcam).place(relx=.5, rely=.4,anchor= N)
+root.mainloop()
+#video_pose_estimation2('vid1.mp4')
--- LastFuncCalMain.py
+++ LastFuncCalMain.py
@@ -46,6 +46,8 @@ def video_pose_estimation2(name):
     # Initialize MediaPipe pose model
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose()
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
     # Define indices of keypoints for drawing lines (connections)
     keypoint_connections = [
