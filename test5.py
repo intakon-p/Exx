5fd195e0f3987b0d07445e124a577e7cbeefb8e0
D123568749 <87933093+D123568749@users.noreply.github.com>
2024-05-17 14:49:48 +0700

    หฟกดฟหอฟห
    อฟอหฟอหฟอหฟหฟหฟอ

--- test5.py
+++ test5.py
@@ -2,6 +2,7 @@ import customtkinter
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import cv2
+from tkVideoPlayer import TkinterVideo
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
@@ -27,8 +28,85 @@ subfram2=customtkinter.CTkFrame(master=rightframe,width=500,height=180,border_co
 subfram2.pack(side="bottom",expand=False,fill="both")
 
 
+#############################################################################################33
+def open_video():
+    # for widget in frame_1.winfo_children():
+    #     widget.destroy()
+    vid_player.stop()
+    global video_file
+    video_file=filedialog.askopenfilename(filetypes =[('Video', ['*.mp4','*.avi','*.mov','*.mkv','*gif']),('All Files', '*.*')])
+    if video_file:
+        try:
+            vid_player.load(video_file)
+            vid_player.play()
+            progress_slider.set(-1)
+            play_pause_btn.configure(text="Pause ||")
+        except:
+            print("Unable to load the file")
+
+def update_duration(event):
+    try:
+        duration = int(vid_player.video_info()["duration"])
+        progress_slider.configure(from_=-1, to=duration, number_of_steps=duration)
+    except:
+        pass
+    
+def seek(value):
+    if video_file:
+        try:
+            
+            vid_player.seek(int(value))
+            vid_player.play()
+            vid_player.after(50,vid_player.pause)
+            play_pause_btn.configure(text="Play ►")
+        except:
+            pass
+    
+def update_scale(event):
+    try:
+        progress_slider.set(int(vid_player.current_duration()))
+    except:
+        pass
+    
+def play_pause():
+    if video_file:
+        if vid_player.is_paused():
+            vid_player.play()
+            play_pause_btn.configure(text="Pause ||")
+
+        else:
+            vid_player.pause()
+            play_pause_btn.configure(text="Play ►")
+        
+def video_ended(event):
+    play_pause_btn.configure(text="Play ►")
+    progress_slider.set(-1)
 
 
+
+video_file=''
+frame_1 = customtkinter.CTkFrame(master=subframe, corner_radius=15,border_color="green")
+frame_1.pack(pady=20, padx=20, fill="both", expand=True)
+
+button_1 = customtkinter.CTkButton(master=leftframe, text="Open Video", corner_radius=8, command=open_video)
+button_1.place(relx=0.5, rely=0.3, anchor="n") 
+
+vid_player = TkinterVideo(master=frame_1, scaled=True, keep_aspect=True, consistant_frame_rate=True, bg="black",)
+vid_player.set_resampling_method(1)
+vid_player.pack(expand=True, fill="both", padx=10, pady=10)
+vid_player.bind("<<Duration>>", update_duration)
+vid_player.bind("<<SecondChanged>>", update_scale)
+vid_player.bind("<<Ended>>", video_ended)
+
+progress_slider = customtkinter.CTkSlider(master=frame_1, from_=-1, to=1, number_of_steps=1, command=seek)
+progress_slider.set(-1)
+progress_slider.pack(fill="both", padx=10, pady=10)
+
+play_pause_btn = customtkinter.CTkButton(master=frame_1, text="Play ►", command=play_pause)
+play_pause_btn.pack(pady=10)
+    
+
+######################################################################################################3
 
 
 def browse_image():
@@ -38,22 +116,26 @@ def browse_image():
 
 # Function to resize and display the image
 def resize_image(file_path):
+    
     img = Image.open(file_path)
     img = img.resize((800, 600), Image.LANCZOS)
     ctk_image = customtkinter.CTkImage(light_image=img, dark_image=img, size=(500, 540))
     
     # Clear previous content in subframe
-    for widget in subframe.winfo_children():
-        widget.destroy()
+    # for widget in subframe.winfo_children():
+    #     widget.destroy()
     
     # Create a label to display the image
-    label = customtkinter.CTkLabel(master=subframe, image=ctk_image, text="")
+    label = customtkinter.CTkLabel(master=frame_1, image=ctk_image, text="")
       # Keep a reference to avoid garbage collection
-    label.pack(expand=True, fill="both")
+    label.place(relx=0.5, rely=0.5, anchor="center")
 
 
 # Function to capture video from webcam and display it in real-time
 def display_video():
+    
+    for widget in subframe.winfo_children():
+        widget.destroy()
     cap = cv2.VideoCapture(0)  # Index 0 represents the default webcam
 
     while True:
@@ -69,16 +151,16 @@ def display_video():
         pil_image = Image.fromarray(frame_rgb)
         
         # Resize the image to fit the subframe dimensions
-        resized_image = pil_image.resize((subframe.winfo_width(), subframe.winfo_height()), Image.ANTIALIAS)
+        resized_image = pil_image.resize((800, 600), Image.LANCZOS)
         
         # Convert the PIL image to a format suitable for customtkinter
         ctk_image = ImageTk.PhotoImage(resized_image)
         
         # Create a label to display the image in the subframe
         if not hasattr(display_video, 'label'):  # Check if label has been created
-            display_video.label = customtkinter.CTkLabel(master=subframe, image=ctk_image, text="")
+            display_video.label = customtkinter.CTkLabel(master=frame_1, image=ctk_image, text="")
             display_video.label.image = ctk_image  # Keep a reference to avoid garbage collection
-            display_video.label.pack(expand=True, fill="both")
+            display_video.label.place(relx=0.5, rely=0.5, anchor="center")
         else:
             display_video.label.configure(image=ctk_image)  # Update the image in the existing label
         
@@ -99,3 +181,8 @@ b2.place(relx=0.5, rely=0.2, anchor="n")
 # button = customtkinter.CTkButton(master=leftframe, text="CTkButton", command=button_function)
 # button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 app.mainloop()
+
+
+
+
+
